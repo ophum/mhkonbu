2e6a8159d0643783fa6680c10075fbe2b391394f
hum_op <inagaki0106@gmail.com>
2019-03-15 00:10:06 +0900

    [change] change indent rule (tab -> 2 spaces)

--- main.c
+++ main.c
@@ -6,74 +6,74 @@
 
 int main(int argc, char **argv)
 {
-	char *filename;
-	FILE *fp;
-	int col = 0;
-	int cnt;
-	int ch;
+  char *filename;
+  FILE *fp;
+  int col = 0;
+  int cnt;
+  int ch;
 
-	if(argc < 2)
-	{
-		return 1;
-	}
+  if(argc < 2)
+  {
+    return 1;
+  }
 
-	filename = argv[1];
+  filename = argv[1];
 
-	if((fp = fopen(filename, "r")) == NULL)
-	{
-		return 1;
-	}
+  if((fp = fopen(filename, "r")) == NULL)
+  {
+    return 1;
+  }
 
-	while((ch = fgetc(fp)) != EOF)
-	{
-		if(col == 0 && ch == ' ') continue;
-		
-		switch(ch)
-		{
-			case '\n': col = 0; break;
-			case '#':
-				for(cnt = 1; (ch = fgetc(fp)) == '#'; cnt++);
-				printf("<h%d>%c", cnt, ch);			
-				while((ch = fgetc(fp)) != EOF && ch != '\n'){
-					putchar(ch);
-				}
-				printf("</h%d>\n", cnt);
-				break;
-			default:
-				printf("<p>");
-				cnt = 0;
-				while((ch = fgetc(fp)) != EOF)
-				{
-					if(ch == '\n')
-					{
-						if(cnt == 0) break;
+  while((ch = fgetc(fp)) != EOF)
+  {
+    if(col == 0 && ch == ' ') continue;
+    
+    switch(ch)
+    {
+      case '\n': col = 0; break;
+      case '#':
+        for(cnt = 1; (ch = fgetc(fp)) == '#'; cnt++);
+        printf("<h%d>%c", cnt, ch);			
+        while((ch = fgetc(fp)) != EOF && ch != '\n'){
+          putchar(ch);
+        }
+        printf("</h%d>\n", cnt);
+        break;
+      default:
+        printf("<p>");
+        cnt = 0;
+        while((ch = fgetc(fp)) != EOF)
+        {
+          if(ch == '\n')
+          {
+            if(cnt == 0) break;
 
-						cnt = 0;
-						continue;
-					}
-					switch(ch)
-					{
-						case ' ':
-							ch = fgetc(fp);
-							if(ch == ' ')
-							{
-								printf("<br>");
-							}else {
-								putchar(' ');
-								ungetc(ch, fp);
-							}
-							break;
-						default:
-							putchar(ch);
-							break;
-					}
-					cnt++;
-				}
-				printf("</p>\n");
-				break;
-		}
-	}
+            cnt = 0;
+            continue;
+          }
+          switch(ch)
+          {
+            case ' ':
+              ch = fgetc(fp);
+              if(ch == ' ')
+              {
+                printf("<br>");
+              }else {
+                putchar(' ');
+                ungetc(ch, fp);
+              }
+              break;
+            default:
+              putchar(ch);
+              break;
+          }
+          cnt++;
+        }
+        printf("</p>\n");
+        break;
+    }
+  }
 
-	fclose(fp);
-	return 0;
+  fclose(fp);
+  return 0;
 }
